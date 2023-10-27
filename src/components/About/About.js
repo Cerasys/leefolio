2437412ae90ae62877869f0a7794c4dfcc7e5065
Nathan Lee <nathanblee@gmail.com>
2023-10-26 22:07:49 -0700

    Update About links

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,10 +12,32 @@ const About = () => {
 
       <div className='about__contact'>
         {resume && (
-          <Button variant='warning' href='mailto:nathanblee@gmail.com'>
-            Get in touch
+          <Button
+            className='mb-2'
+            variant='secondary'
+            href={resume}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Resume
           </Button>
         )}
+        <Button
+          className='mb-2'
+          variant='warning'
+          href='https://nblee.notion.site/bd37d0430b3c4ca5b057ac968a57b7d1?v=4de55da4101147b5a8ad7b9841cf34cb&pvs=4'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Portfolio
+        </Button>
+        <Button
+          className='mb-2'
+          variant='danger'
+          href='mailto:nathanblee@gmail.com'
+        >
+          Get in touch
+        </Button>
       </div>
     </section>
   )
