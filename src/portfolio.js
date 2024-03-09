a33938255dff65c9bdb10d1ab6ee67ff253acecb
Cerasys <nathanblee@gmail.com>
2024-03-08 16:08:41 -0800

    edit to portfolio endpoints

--- src/portfolio.js
+++ src/portfolio.js
@@ -12,18 +12,20 @@ const about = {
   name: 'Nathan Lee',
   role: 'Full stack developer',
   description:
-    '🇺🇸 🇹🇼 entrepreneur passionate about building delightful product experiences, lifelong learning, and wearing different hats across product, design, growth, and operations.',
+    '🇺🇸 🇹🇼 entrepreneur passionate about helping business owners win',
   resume:
     'https://drive.google.com/file/d/1wlu5zUxQfA3eyNtvGQ-cn7LMwt1uxG4k/view?usp=sharing',
   social: {
     linkedin: 'https://www.linkedin.com/in/nlnathanlee/',
     github: 'https://github.com/Cerasys',
-    instagram: 'https://www.instagram.com/nathan.b.lee/',
+    instagram: 'https://www.instagram.com/nthnblee/',
     spotify: 'https://open.spotify.com/user/aixora?si=91a1d3cbdf224e1c',
   },
 }
 
 const portfolio = 'https://nblee.notion.site/bd37d0430b3c4ca5b057ac968a57b7d1'
+
+const business = 'https://prescelta.com'
 
 const projects = []
 
@@ -31,7 +33,7 @@ const skills = []
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'nathanblee@gmail.com',
+  email: 'nathan@prescelta.com',
 }
 
-export { header, about, projects, skills, contact, portfolio }
+export { header, business, about, projects, skills, contact, portfolio }
