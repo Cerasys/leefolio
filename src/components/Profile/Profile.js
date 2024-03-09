a7eab33d70818b9922adb494331f06af49abb8e3
Cerasys <nathanblee@gmail.com>
2024-03-08 16:15:26 -0800

    profile changes

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -5,7 +5,7 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image'
 
-import { about } from '../../portfolio'
+import { about, business } from '../../portfolio'
 
 const Profile = () => {
   const { photo } = about
@@ -23,61 +23,72 @@ const Profile = () => {
             />
           </Col>
           <Col sm={6} xl={7}>
-            <div className='text-container'>
-              <p>
-                Starting my first business in college, I’ve been fascinated by
-                teams coming together and creating something from nothing.
-              </p>
-              <p>
-                I’ve been working as a Product Manager for the past 4 years
-                supporting business leaders ranging from global nonprofits to
-                big names in recruiting.
-              </p>
-              <p>
-                I’ve founded multiple companies, with varying degrees of
-                success, since college and have experience building networked
-                products, creating AI solutions, and running companies.
-              </p>
-              <p>Some of my proudest work accomplishments include:</p>
+            <Row>
+              <div className='text-container'>
+                <p>
+                  Having started my first business in college, I’ve been
+                  fascinated by the physics of business-the universal rules that
+                  are true across industries and business models.
+                </p>
+                <p>
+                  A CEO and Product Leader for over 4 years, I’ve founded
+                  multiple companies and supported business leaders ranging from
+                  global nonprofits to AI startups.
+                </p>
+                <p>Some of my proudest work accomplishments include:</p>
 
-              <ul className='ul-profile'>
-                <li>
-                  Launching{' '}
-                  <a
-                    href='https://nblee.notion.site/PersonaAI-CPO-CoFounder-fd3ccabef7cc4d22a084b984a5dfa444'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                  >
-                    PersonaAI
-                  </a>{' '}
-                  (startup), an AI interview tool for recruiting agencies.
-                </li>
-                <li>
-                  Overhauling{' '}
-                  <a
-                    href='https://nblee.notion.site/Healing-Rooms-Ministries-PM-cca22ee3243e4c5488918ba006ca1927'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                  >
-                    Healing Rooms Ministries
-                  </a>
-                  , a Christian ministry whose techstack hadn’t been touched in
-                  nearly 25 years.
-                </li>
-                <li>
-                  Created and Kickstarted a board game called{' '}
-                  <a
-                    href='https://nblee.notion.site/JailBird-CEO-CoFounder-847c7ba0591543cb80648771d831e754'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                  >
-                    JailBird
-                  </a>{' '}
-                  while in college, selling over 5-figures of product in our
-                  first year.
-                </li>
-              </ul>
-            </div>
+                <ul className='ul-profile'>
+                  <li>
+                    <b>
+                      <a
+                        href={business}
+                        target='_blank'
+                        rel='noopener noreferrer'
+                      >
+                        [CURRENTLY BUILDING]: The ultimate bootcamp for
+                        therapreneurs (therapist entrepreneur) to launch their
+                        private practice.
+                      </a>
+                    </b>{' '}
+                  </li>
+                  <li>
+                    Launching{' '}
+                    <a
+                      href='https://nblee.notion.site/PersonaAI-CPO-CoFounder-fd3ccabef7cc4d22a084b984a5dfa444'
+                      target='_blank'
+                      rel='noopener noreferrer'
+                    >
+                      PersonaAI
+                    </a>{' '}
+                    (startup), an AI interview tool for recruiting agencies.
+                  </li>
+                  <li>
+                    Overhauling{' '}
+                    <a
+                      href='https://nblee.notion.site/Healing-Rooms-Ministries-PM-cca22ee3243e4c5488918ba006ca1927'
+                      target='_blank'
+                      rel='noopener noreferrer'
+                    >
+                      Healing Rooms Ministries
+                    </a>
+                    , a Christian ministry whose techstack hadn’t been touched
+                    in nearly 25 years.
+                  </li>
+                  <li>
+                    Created and Kickstarted a board game called{' '}
+                    <a
+                      href='https://nblee.notion.site/JailBird-CEO-CoFounder-847c7ba0591543cb80648771d831e754'
+                      target='_blank'
+                      rel='noopener noreferrer'
+                    >
+                      JailBird
+                    </a>{' '}
+                    while in college, selling over 5-figures of product in our
+                    first year.
+                  </li>
+                </ul>
+              </div>
+            </Row>
           </Col>
         </Row>
       </Container>
